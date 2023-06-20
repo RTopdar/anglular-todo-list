7b4cc44d91d50193eccb4c308917865b7c4e2801
Rounak Topdar <rounak2002topdar@gmail.com>
2023-06-20 13:59:55 +0530

    slowly seeting up angular

---
+++ src/app/MyComponents/todos/todos.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import{Todo} from "../../Todo"
+@Component({
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.css']
+})
+export class TodosComponent {
+todos: Todo[];
+
+}
